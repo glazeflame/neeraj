034fbbcf4e3c531862b2185f91ac502800ca5252
glazeflame <122000558+glazeflame@users.noreply.github.com>
2023-01-05 11:22:12 +0530

    Add files via upload

---
+++ EvenOrOdd.py
@@ -0,0 +1,9 @@
+class Number:
+    num=5
+    def EvenOrOdd():
+        if(num%2==0):
+            print("Number is Even")
+        else:
+            print("Number is Odd")
+x=Number()
+x.EvenOrOdd()
